2e583e9f6ce69fe189df9903e560515266cef1c9
Cesare Ferrari <cesare@maryandferrari.com>
2019-03-27 10:59:05 -0400

    Find last car in arrays challenges

--- assignments/arrays.js
+++ assignments/arrays.js
@@ -78,12 +78,15 @@ console.log(`Car 33 is a ${car33.car_year} ${car33.car_make} ${car33.car_model}`
 
 
 
-
-
 // ==== Challenge 2 ====
 // The dealer needs the information on the last car in their inventory.  What is the make and model of the last car in the inventory?  Log the make and model into the console.
-let lastCar = 0;
-console.log();
+
+let lastCar = inventory[inventory.length - 1];
+
+console.log('Last car make:', lastCar.car_make);
+console.log('Last car model:', lastCar.car_model);
+
+
 
 // ==== Challenge 3 ====
 // The marketing team wants the car models listed alphabetically on the website. Sort all the car model names into alphabetical order and log the results in the console
