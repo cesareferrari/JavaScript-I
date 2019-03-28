b1dd4e5e69fdefda2933e69c97f4879371d0c7f4
Cesare Ferrari <cesare@maryandferrari.com>
2019-03-27 21:49:59 -0400

    Implement Array Challenge 5 using Array.filter

--- assignments/array-methods.js
+++ assignments/array-methods.js
@@ -118,15 +118,19 @@ console.log(carYears);
 
 // ==== Challenge 5 ====
 // The car lot manager needs to find out how many cars are older than the year 2000. Using the carYears array you just created, find out how many cars were made before the year 2000 by populating the array oldCars and logging it's length.
-let oldCars = [];
 
-for (let i = 0; i < carYears.length; i++) {
-  let year = carYears[i];
-  if (year < 2000) {
-    oldCars.push(year);
-  }
-}
 
+// let oldCars = [];
+// 
+// for (let i = 0; i < carYears.length; i++) {
+//   let year = carYears[i];
+//   if (year < 2000) {
+//     oldCars.push(year);
+//   }
+// }
+
+
+const oldCars = carYears.filter(year => year < 2000);
 console.log('Number of old cars:', oldCars.length); 
 
 
